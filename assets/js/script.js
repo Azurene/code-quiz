d2569863679e29865d3cfcc0e46de5dd94a2613a
Azurene <98633770+Azurene@users.noreply.github.com>
2022-06-13 12:54:32 -0700

    added quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,44 @@
 var timerEl = document.getElementById('countdown');
 var quizEl = document.getElementById('quiz');
+var startButtonEl = document.getElementById('start')
 
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ___.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
+
+// function to start quiz
+var startQuiz = function() {
+    countdownTimer();
+    console.log("start!");
+}
+
+// function for the timer
 var countdownTimer = function() {
     var timeLeft = 5;
-
     var timeInterval = setInterval(function() {
        if (timeLeft >= 0) {
            timerEl.textContent = timeLeft;
@@ -17,4 +52,4 @@ var countdownTimer = function() {
 };
 
 
-countdownTimer();
+startButtonEl.addEventListener('click', startQuiz)
